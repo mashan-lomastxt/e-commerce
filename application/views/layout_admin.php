d48a22fbe421b6354e1a3e3bd87af0e305785c57
mashan-lomastxt <riska.handika@students.amikom.ac.id>
2021-03-25 17:40:06 +0700

    add icon

--- application/views/layout_admin.php
+++ application/views/layout_admin.php
@@ -56,19 +56,19 @@
                             </div>
                             <div class="search-item">
                                 <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="../assets/img/products/product-3-50.png" alt="product">
+                                    <img class="mr-3 rounded" width="30" src="<?php echo base_url('assets/admin/assets/img/products/product-3-50.png'); ?>" alt="product">
                                     oPhone S9 Limited Edition
                                 </a>
                             </div>
                             <div class="search-item">
                                 <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="../assets/img/products/product-2-50.png" alt="product">
+                                    <img class="mr-3 rounded" width="30" src="<?php echo base_url('assets/admin/assets/img/products/product-2-50.png'); ?>" alt="product">
                                     Drone X2 New Gen-7
                                 </a>
                             </div>
                             <div class="search-item">
                                 <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="../assets/img/products/product-1-50.png" alt="product">
+                                    <img class="mr-3 rounded" width="30" src="<?php echo base_url('assets/admin/assets/img/products/product-1-50.png'); ?>" alt="product">
                                     Headphone Blitz
                                 </a>
                             </div>
